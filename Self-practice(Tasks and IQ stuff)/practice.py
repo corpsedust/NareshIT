fec6c1f5c0f3812448bcb9ea3038b9285de8dfd3
Vineet <enivet0@gmail.com>
2025-03-12 07:50:43 +0530

    eh

--- Self-practice(Tasks and IQ stuff)/practice.py
+++ Self-practice(Tasks and IQ stuff)/practice.py
@@ -16,7 +16,7 @@ name_label = tk.Label(app,text = "Enter a name")
 name_label.pack(padx = 10, pady = 10)
 
 name_entry = tk.Entry(app, text = "Jai ho")
-# name_entry.pack(padx = 10, pady = 10)
+name_entry.pack(padx = 10, pady = 10)
 
 button = tk.Button(app, text = "Bhai Dabao", overrelief="raised")
 button.pack(padx = 10, pady = 10)
