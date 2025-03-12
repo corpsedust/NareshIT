b9772eb89b36a732b453c110166b661c769b34a4
Vineet <enivet0@gmail.com>
2025-03-12 08:03:51 +0530

    app.py

--- Self-practice(Tasks and IQ stuff)/practice.py
+++ Self-practice(Tasks and IQ stuff)/practice.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import *
+from tkinter import messagebox
 
 
 
@@ -7,9 +7,12 @@ app = tk.Tk()
 app.title("Simple App hue hue")
 app.geometry("400x400")
 
-# def something():
-#     name = name_entry
-#     if name:
+def something():
+    name = name_entry.get()
+    if name:
+        messagebox.showinfo("Bakchodi",message = f"Lol Gay {name}")
+    else:
+        messagebox.showinfo("Tumse na ho paayega", message= "Rehene de Bhai")
         
 
 name_label = tk.Label(app,text = "Enter a name")
@@ -18,7 +21,7 @@ name_label.pack(padx = 10, pady = 10)
 name_entry = tk.Entry(app, text = "Jai ho")
 name_entry.pack(padx = 10, pady = 10)
 
-button = tk.Button(app, text = "Bhai Dabao", overrelief="raised")
+button = tk.Button(app, text = "Bhai Dabao", overrelief="raised", command = something)
 button.pack(padx = 10, pady = 10)
 
 app.mainloop()
