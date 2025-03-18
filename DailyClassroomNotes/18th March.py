cd37915938dfdf7b504678f04ea60bfd2ef99fbf
Vineet <enivet0@gmail.com>
2025-03-18 20:12:33 +0530

    sklearn project

---
+++ DailyClassroomNotes/18th March.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 18 19:41:40 2025
+
+@author: vineet
+"""
+
+
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt
+
+
+#Import the dataset
+df = pd.read_csv(r"C:\Users\vineet\Desktop\Pyhtonshit\DailyClassroomNotes\Data_18March.csv")
+
+
+#Split the dataset;
+x = df.iloc[:,:-1].values
+y = df.iloc[:,3].values
+
+
+#filling missing values
+
+from sklearn.impute import SimpleImputer
+
+imputer = SimpleImputer()
+
