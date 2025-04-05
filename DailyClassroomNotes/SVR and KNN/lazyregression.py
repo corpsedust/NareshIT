60fca9d9454a904f255e3baeaac4e22a0dd43bbf
Vineet <enivet0@gmail.com>
2025-04-05 13:00:54 +0530

    datadata

--- DailyClassroomNotes/SVR and KNN/lazyregression.py
+++ DailyClassroomNotes/SVR and KNN/lazyregression.py
@@ -9,4 +9,11 @@ df = pd.read_csv("emp_sal.csv")
 x = df.iloc[:,1:2].values
 y = df.iloc[:,-1].values
 
-from lazyrpredict.Supervised import  LazyRegressor 
+from lazypredict.Supervised import  LazyRegressor
+
+from sklearn.model_selection import train_test_split
+
+x_train, y_train, x_test, y_test = train_test_split(x,y, train_size= 0.8)
+
+reg = LazyRegressor(verbose = 0, ignore_warnings = False, custom_metric = None)
+models, prediction = reg.fit(x_train, x_test, y_train, y_test)
