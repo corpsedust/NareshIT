37864b05a028262ffc60b607c28ea0fcafd51440
Vineet <enivet0@gmail.com>
2025-04-09 19:29:25 +0530

    free

--- DailyClassroomNotes/logistic regression/appoop.py
+++ DailyClassroomNotes/logistic regression/appoop.py
@@ -20,9 +20,11 @@ y = df.iloc[:,-1].values
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.8, random_state = 0)
 
+#Either Normalizer or Standardize the data
+#from sklearn.preprocessing import Normalizer
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
-
+#sc = Normalizer()
 
 x_train = sc.fit_transform(x_train)
 x_test = sc.fit_transform(x_test)
@@ -54,4 +56,53 @@ print(f"Variance : {variance}")
 from sklearn.metrics import classification_report
 cr = classification_report(y_test, y_pred)
 print(cr)
+
+#Case1 : Train_size = 0.8, StandardScaler, Accuracy : 88.75, bias : 0.8875, variance = 0.821875
+#Case 2: Train_size = 0.8, Normalizer, Accuracy : 72.5, bias : 0.725, variance = 0.621875
+
+#Case 3: Train_size = 0.75, Normalizer, Accuracy : 68, bias : 0.68, variance = 0.63
+#Case 4: Train_size = 0.75, StandardScaler, Accuracy : 87, bias : 0.87, variance = 0.8233333333333334
+
+#Case 5: Train_size = 0.8, No Normalisztion or Standardization, Accuracy : 91.25, bias : 0.9125, variance = 0.81875
+
+
+#Prediction of unlabelled data (Whether purchase or not) 
+#We have a classifier model on historical data
+
+
+future_data = pd.read_csv("final1.csv")
+copy = future_data
+x_future = future_data.iloc[:,[3,4]].values
+x_future = sc.fit_transform(x_future)
+y_future = classifier.predict(x_future)
+
+
+#new_df = pd.DataFrame({"purchase or not" : y_future})
+copy["Purchased or Not"] = y_future
+copy["Purchased or Not"] = copy["Purchased or Not"].map({0:"NO", 1:"YES"})
+copy.to_csv("Predicted_Data.csv")
+
+
+#Dependency of random_state and accuracy ?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
