454964a813765f2e616d40ef08f1d4006d2341d2
Vineet <enivet0@gmail.com>
2025-04-11 20:02:14 +0530

    koko

---
+++ DailyClassroomNotes/KNN_Classification/Knn_classification.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+#Importing the dataset
+
+df = pd.read_csv(r"C:\Users\vineet\Desktop\Pyhtonshit\DailyClassroomNotes\logistic regression\logit classification.csv")
+
+x = df.iloc[:,[2,3]].values
+y = df.iloc[:,-1].values
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x,y,train_size=0.80, random_state = 0)
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+#sc = Normalizer()
+
+x_train = sc.fit_transform(x_train)
+x_test = sc.fit_transform(x_test)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=5, weights = "distance")
+knn.fit(x_train, y_train)
+
+
+y_pred = knn.predict(x_test)
+
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+cm = confusion_matrix(y_test, y_pred)
+print("Confusion Matrix : \n", cm)
+
+ac = accuracy_score(y_test, y_pred)
+print(f"Accuracy Score : {ac}")
+
+variance = knn.score(x_train, y_train)
+print(f"Variance : {variance}")
+
+
+cr = classification_report(y_test, y_pred)
+print(cr)
