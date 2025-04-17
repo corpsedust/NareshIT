a2fb6eafd37e2c31b62694bdefc3a3ac31ae2dea
Vineet <enivet0@gmail.com>
2025-04-17 19:58:07 +0530

    (*-_-)

--- PythontoMSQLconnector/first.py
+++ PythontoMSQLconnector/first.py
@@ -5,3 +5,12 @@ conn = mysql.connector.connect(host = "localhost", user = "root", password = "En
 if conn.is_connected():
     print("Connection Established")
 print()
+
+mycursor = conn.cursor()
+mycursor.execute('show databases')
+myresult = mycursor.fetchall()
+for x in mycursor:
+    print(mycursor)
+    
+    
+mycursor.execute('create database pythondb')
