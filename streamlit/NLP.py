61416cfc7c32de47d166277e2eb3bf877f99bec9
Vineet <enivet0@gmail.com>
2025-05-05 14:32:21 +0530

    slow

--- streamlit/NLP.py
+++ streamlit/NLP.py
@@ -3,7 +3,12 @@ import streamlit as st
 from nltk.tokenize import word_tokenize, sent_tokenize, blankline_tokenize, WhitespaceTokenizer, wordpunct_tokenize
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-
+from nltk.stem import PorterStemmer, LancasterStemmer, SnowballStemmer, WordNetLemmatizer
+from nltk.corpus import stopwords
+pst = PorterStemmer()
+lcs = LancasterStemmer()
+sbst = SnowballStemmer(language="english")
+word_net = WordNetLemmatizer()
 
 st.title("Natural language Understanding")
 st.subheader("Tokenization")
@@ -62,3 +67,49 @@ if wc_button:
     plt.margins(x=0, y=0)
     # plt.show()
     st.pyplot(fig)
+
+st.subheader('Stemming')
+st.text("Select one of the following stemmer to see result for each word in the text box above")
+
+
+
+bt1 = st.button(label = 'LancasterStemmer')
+bt2 = st.button(label = 'PorterStemmer')
+bt3 = st.button(label = 'SnowballStemmer')
+
+
+word_tokens = nltk.word_tokenize(text)
+stop_words = stopwords.words('english')
+
+filtered_sentence = [w for w in word_tokens if w.lower() not in stop_words]
+dic = {}
+
+
+if bt1:
+    for i in filtered_sentence:
+        dic[i] = lcs.stem(i)
+    st.write(dic)
+    
+if bt2:
+    for i in filtered_sentence:
+        dic[i] = pst.stem(i)
+    st.write(dic)
+    
+if bt3:
+    for i in filtered_sentence:
+        dic[i] = sbst.stem(i)
+    st.write(dic)
+    
+    
+    
+
+st.subheader('Lemmmatization')
+
+st.text("Press the button to Lemmatize the text")
+bt4 = st.button(label = "WordNetLemmatizer")
+
+if bt4:
+    for i in filtered_sentence:
+        dic[i] = word_net.lemmatize(i)
+    st.write(dic)
+        
