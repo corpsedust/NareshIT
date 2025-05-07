bbfd3d8882d775aca45c9bc2650b735b2ae43141
Vineet <enivet0@gmail.com>
2025-05-07 19:49:13 +0530

    koala eat water

---
+++ Self-practice/Customer Review (NLP + Classification)/untitled0.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  6 19:22:10 2025
+
+@author: vineet
+"""
+
+import numpy as np 
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv(r"C:\Users\vineet\Desktop\textcleanup\Restaurant_Reviews.tsv", delimiter="\t")
+
+
+#Cleaning the text - lol
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+
+corpus = []
+
+for i in range(1, 1000):
+    review = re.sub('[^a-zA-Z]',' ',dataset['Review'][i])
+    review = review.lower()
+    ps = PorterStemmer()
+    review = [ps.stem(word) for word in review if word not in stopwords]
+    review = ''.join(review)
+    corpus.append(review)
+
+
